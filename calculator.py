337bb01ebed3cc135fcd1f99a74d31f276df7cfb
SSomagagu <ssomagagu@gmail.com>
2026-02-17 01:14:57 +0200

    Created the calculate function

--- calculator.py
+++ calculator.py
@@ -1,29 +1,63 @@
 import re
 from typing import Any
+from threading import Lock
 
 
-VALID_EXPRESSION_PATTERN: str = r"(([+-]*|\*{0,2}|/)?(\d+\.?|.?\d+|\d+\.?\d+))+"
+NUM_PATTERN: str = r"([+-]{2,}(?:\d+\.|\.\d+|\d+\.\d+|\d+))"
+
+
 class Calculator():
-    @staticmethod
-    def get_valid(expr: str, vars: dict[str, str] = None) -> Any:
-        if vars is None:
-            vars = {}
+    def __init__(self, lock: Lock = None) -> None:
+        self.preans: int = 0
+        self.ans: int = 0
+        self.lock = lock
 
-        for res in re.findall("[0-9.]preans", expr):
-            expr = expr.replace(res, f"*{res}")
-        for res in re.findall(r"preans[0-9.]", expr):
-            expr = expr.replace(res, f"*{res}")
 
-        expr = expr.replace(
-            "preans", vars.get("preans", '0')
-        )
+    def get_valid(self, num: str) -> str:
+        if not num:
+            raise ValueError(
+                "Num is invalid."
+            )
+        num: str = num.replace('+', '')
+        return num.replace("--", '')
 
-        for var, value in vars:
-            for res in re.findall(f"[0-9.]{var}", expr):
-                expr = expr.replace(var, f"*{var}")
-            for res in re.findall(f"{var}[0-9.]", expr):
-                expr = expr.replace(var, f"{var}*")
-            expr = expr.replace(var, f"{value}")
 
-        if re.fullmatch(VALID_EXPRESSION_PATTERN, expr):
-            return expr
+    def calculate(self, expression: list[str]) -> Any:
+        if self.lock:
+            self.lock.acquire()
+        expr = ''.join(expression)
+        for var in ("preans", "ans"):
+            for c in re.findall(f"[0-9]{var}", expr):
+                expr = expr.replace(c, f"{c[0]}*{var}")
+            for c in re.findall(f"{var}[0-9]", expr):
+                expr = expr.replace(c, f"{var}*{c[-1]}")
+            expr = expr.replace(var,
+                f"{getattr(self, var)}"
+            )
+        for c in re.findall(r"[0-9]\(", expr):
+            expr = expr.replace(c, f"{c[0]}*(")
+
+        for c in re.findall(r"\)[0-9]", expr):
+            expr = expr.replace(c, f")*{c[-1]}")
+
+        for match in re.findall(NUM_PATTERN, expr):
+            expr = expr.replace(match, self.get_valid(match))
+
+        try:
+            return eval(expr), "Successful"
+
+        except ZeroDivisionError as e:
+            return None, "ComplexInfinity"
+        except Exception as e:
+            return None, ""
+
+        finally:
+            if self.lock:
+                self.lock.release()
+
+if __name__ == "__main__":
+    calc: Calculator = Calculator()
+    expr: str = "+-+67**2-7(78)"
+    print(expr)
+    print(calc.calculate(list(expr)))
+
